34c79ab54b6fa648bffc3ee5592a40b6ca96255f
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2019-11-19 07:39:19 +0100

    Add main navigation items to pages

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import Link from '@material-ui/core/Link';
+import { withPrefix } from 'gatsby-link';
+import {
+  injectIntl,
+  FormattedMessage,
+  InjectedIntlProps,
+  // @ts-ignore
+} from 'gatsby-plugin-intl';
 import Typography from '@material-ui/core/Typography';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 type Props = {
   siteTitle: string;
-};
+} & InjectedIntlProps;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,15 +30,63 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 function Header(props: Props) {
-  const { siteTitle } = props;
+  const { siteTitle, intl } = props;
+  const { locale } = intl;
   const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar position="static" color="default" className={classes.root}>
       <Toolbar>
-        <IconButton color="inherit" aria-label="Menu">
+        <IconButton
+          color="inherit"
+          aria-label="Menu"
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
           <MenuIcon />
         </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={handleClose}
+            component={Link}
+            href={withPrefix(`/${locale}/`)}
+          >
+            <FormattedMessage id="site.navigation.home" />
+          </MenuItem>
+
+          <MenuItem
+            onClick={handleClose}
+            component={Link}
+            href={withPrefix(`/${locale}/typewriter/`)}
+          >
+            <FormattedMessage id="site.navigation.program" />
+          </MenuItem>
+
+          <MenuItem
+            onClick={handleClose}
+            component={Link}
+            href={withPrefix(`/${locale}/statistics`)}
+          >
+            <FormattedMessage id="site.navigation.statistics" />
+          </MenuItem>
+        </Menu>
         <Typography variant="h6" color="inherit">
           {siteTitle}
         </Typography>
@@ -37,4 +95,4 @@ function Header(props: Props) {
   );
 }
 
-export default Header;
+export default injectIntl(Header);
