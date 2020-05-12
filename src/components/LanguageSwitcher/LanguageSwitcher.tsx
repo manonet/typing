01a2d626dbbec9f192e7468df691f7b2988d6097
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2020-05-12 12:30:03 +0200

    Update translations
    * Add Polish translations
    * Add Croatian translations
    * Add empty JSON files to make Poeditor integration easier

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -14,9 +14,43 @@ type Props = {
 };
 
 const languageName = {
+  ar: 'العربية', // Arabic
+  bg: 'Български', // Bulgarian
+  bs: 'Bosanski', // Bosnian
+  cs: 'Čeština', // Czech
+  da: 'Dansk', // Danish
+  de: 'Deutsch', // German
+  el: 'Ελληνικά', // Greek
   en: 'English',
-  hu: 'Magyar',
-  de: 'Deutsch',
+  es: 'Español', // Spanish
+  et: 'Eesti', // Estonian
+  fa: 'فارسی', // Persian
+  fi: 'Suomi', // Finnish
+  fr: 'Français', // French
+  hi: 'हिन्दी', // Hindi
+  hr: 'Hrvatski', // Croatian
+  hu: 'Magyar', // Hungarian
+  it: 'Italiano', // Italian
+  ja: '日本語', // Japanese
+  lt: 'Lietuvių', // Lithuanian
+  lv: 'Latviešu', // Latvian
+  mk: 'Македонски', // Macedonian
+  ml: 'മലയാളം', // Malayalam
+  nl: 'Nederlands', // Dutch
+  pa: 'ਪੰਜਾਬੀ', // Punjabi
+  pl: 'Polski', // Polish
+  pt: 'Português', // Portuguese
+  ro: 'Română', // Romanian
+  ru: 'Русский', // Russian
+  sk: 'Slovenčina', // Slovak
+  sl: 'Slovenščina', // Slovenian
+  sq: 'Shqip', // Albanian
+  sr: 'Српски / srpski', // Serbian
+  sv: 'Svenska', // Swedish
+  te: 'తెలుగు', // Telugu
+  tr: 'Türkçe', // Turkish
+  uk: 'Українська', // Ukrainian
+  zh: '中文', // Chinese
 };
 
 export interface IntlContextConsumerProps {
