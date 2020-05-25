3d2276904a46c29a8773c9a1ebef03c65d8b72f8
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2020-05-25 19:53:26 +0200

    Fix arrayContainsAnotherArray function

--- src/utils/arrayContainsAnotherArray.ts
+++ src/utils/arrayContainsAnotherArray.ts
@@ -2,6 +2,9 @@ export default function arrayContainsAnotherArray(
   needle: string[],
   haystack: string[]
 ) {
+  if (!needle.length || !haystack.length) {
+    return false;
+  }
   for (let i = 0; i < needle.length; i++) {
     if (haystack.indexOf(needle[i]) === -1) {
       return false;
