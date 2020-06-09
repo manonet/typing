5f10907d1ca5ca2362c99c7b69d9663c275013f9
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2020-06-09 15:35:51 +0200

    Add shuffleArray util function

---
+++ src/utils/__tests__/shuffleArray.test.ts
@@ -0,0 +1,23 @@
+import shuffleArray from '../shuffleArray';
+
+describe('shuffleArray', () => {
+  global.Math.random = () => 0.5;
+
+  it('returns an empty array, if empty array is given', () => {
+    const shuffledArray = shuffleArray([]);
+
+    expect(shuffledArray).toEqual([]);
+  });
+
+  it('returns the same array, if the array has only one item', () => {
+    const shuffledArray = shuffleArray(['a']);
+
+    expect(shuffledArray).toEqual(['a']);
+  });
+
+  it('returns a shuffled array', () => {
+    const shuffledArray = shuffleArray(['a', 'b', 'c']);
+
+    expect(shuffledArray).toEqual(['a', 'c', 'b']);
+  });
+});
---
+++ src/utils/shuffleArray.ts
@@ -0,0 +1,9 @@
+const shuffleArray = function (a: string[]) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
+
+export default shuffleArray;
