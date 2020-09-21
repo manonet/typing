5e9fff7a6ee388d03c89ebe56bb16939aabc44b1
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2020-09-21 19:57:12 +0200

    Remove redux-persist
    This reverts 1d6d26cf
    
    Closes: #29

--- src/state/ReduxWrapper.tsx
+++ src/state/ReduxWrapper.tsx
@@ -1,29 +1,19 @@
 import React, { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createStore as reduxCreateStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import rootReducer from '../reducers';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const createStore = () => {
   if (typeof window === 'object') {
     return reduxCreateStore(
-      persistedReducer,
+      rootReducer,
       (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
         (window as any).__REDUX_DEVTOOLS_EXTENSION__()
     );
   }
 
-  return reduxCreateStore(persistedReducer);
+  return reduxCreateStore(rootReducer /* TODO - , initialState */);
 };
 
 // TODO what's the proper type?
@@ -32,15 +22,8 @@ export default ({ element }: { element: ReactNode }) => {
   //  - there is fresh store for each SSR page
   //  - it will be called only once in browser, when React mounts
   const store = createStore();
-  const persistor = persistStore(store);
 
   console.info('Happy hacking! (۶ૈ ಠ益ಠ)۶ૈ=͟͟͞͞ ⌨');
 
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {element}
-      </PersistGate>
-    </Provider>
-  );
+  return <Provider store={store}>{element}</Provider>;
 };
