8159f36690220b0d2c9db3dbd82e0b0c165ab934
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2020-12-27 14:30:16 +0100

    Add redux-persist
    * Some storage is necessary for publishing the app

--- src/state/ReduxWrapper.tsx
+++ src/state/ReduxWrapper.tsx
@@ -1,19 +1,29 @@
 import React, { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createStore as reduxCreateStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import rootReducer from '@reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const createStore = () => {
   if (typeof window === 'object') {
     return reduxCreateStore(
-      rootReducer,
+      persistedReducer,
       (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
         (window as any).__REDUX_DEVTOOLS_EXTENSION__()
     );
   }
 
-  return reduxCreateStore(rootReducer /* TODO - , initialState */);
+  return reduxCreateStore(persistedReducer /* TODO - , initialState */);
 };
 
 // TODO what's the proper type?
@@ -22,8 +32,15 @@ export default ({ element }: { element: ReactNode }) => {
   //  - there is fresh store for each SSR page
   //  - it will be called only once in browser, when React mounts
   const store = createStore();
+  const persistor = persistStore(store);
 
-  console.info('Happy hacking! (۶ૈ ಠ益ಠ)۶ૈ=͟͟͞͞ ⌨');
+  // console.info('Happy hacking! (۶ૈ ಠ益ಠ)۶ૈ=͟͟͞͞ ⌨');
 
-  return <Provider store={store}>{element}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {element}
+      </PersistGate>
+    </Provider>
+  );
 };
