ee5bc2557d15a4abf18bfef49647215d09f8779a
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2020-12-30 13:48:14 +0100

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -14,15 +14,20 @@ const NotFoundPage = () => {
           defaultMessage: 'Not found',
         })}
       />
-      <h1>
-        <FormattedMessage id="404.page.title" defaultMessage="Not found" />
-      </h1>
-      <p>
-        <FormattedMessage
-          id="404.page.desc"
-          defaultMessage="The page you are looking for could not be found."
-        />
-      </p>
+
+      <section className="notFound__section">
+        <div className="container">
+          <h2>
+            <FormattedMessage id="404.page.title" defaultMessage="Not found" />
+          </h2>
+          <p>
+            <FormattedMessage
+              id="404.page.desc"
+              defaultMessage="The page you are looking for could not be found."
+            />
+          </p>
+        </div>
+      </section>
     </Layout>
   );
 };
